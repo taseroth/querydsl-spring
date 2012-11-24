da3de49d91f2b813af6b8fb551e76534f5373988
Eddie <taseroth@gmail.com>
2012-11-24 20:39:02 +0100

    maven archetype output

---
+++ src/main/java/test/faboo/org/App.java
@@ -0,0 +1,13 @@
+package test.faboo.org;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
