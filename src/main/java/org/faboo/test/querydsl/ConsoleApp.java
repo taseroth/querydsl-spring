cc0e3d675c58d10899d4bc17abbbfbaf85b37a30
Eddie <taseroth@gmail.com>
2012-11-25 22:34:42 +0100

    working with QTypes and DTO generation

--- src/main/java/org/faboo/test/querydsl/ConsoleApp.java
+++ src/main/java/org/faboo/test/querydsl/ConsoleApp.java
@@ -1,7 +1,91 @@
 package org.faboo.test.querydsl;
 
+import com.mysema.query.Tuple;
+import com.mysema.query.codegen.BeanSerializer;
+import com.mysema.query.sql.PostgresTemplates;
+import com.mysema.query.sql.SQLQuery;
+import com.mysema.query.sql.SQLQueryImpl;
+import com.mysema.query.sql.SQLTemplates;
+import com.mysema.query.sql.codegen.MetaDataExporter;
+import com.mysema.query.types.Projections;
+import com.mysema.query.types.QTuple;
+import com.mysema.query.types.path.BeanPath;
+import org.faboo.test.querydsl.generated.qtypes.QPlayer;
+import org.faboo.test.querydsl.model.Player;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.List;
+
 /**
  * User: br
  */
 public class ConsoleApp {
+
+    public static void main(String[] args) {
+
+        Connection connection = null;
+
+        try {
+            Class.forName("org.postgresql.Driver").newInstance();
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost/querydsl", "test", "geheim");
+            SQLTemplates dialect = new PostgresTemplates();
+
+            queryWithTuples(connection, dialect);
+            //queryWithBeanProjection(connection,dialect);
+            generateDTO(connection, dialect);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if(connection != null && !connection.isClosed()) {
+                    connection.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+
+    }
+
+    private static void generateDTO(Connection connection, SQLTemplates dialect) throws SQLException {
+
+        MetaDataExporter exporter = new MetaDataExporter();
+        exporter.setPackageName("com.myproject.mydomain");
+        exporter.setTargetFolder(new File("/home/br/temp/src/main/java"));
+        exporter.setBeanSerializer(new BeanSerializer());
+        exporter.export(connection.getMetaData());
+
+    }
+
+    private static void queryWithBeanProjection(Connection connection, SQLTemplates dialect) {
+        SQLQuery query = new SQLQueryImpl(connection, dialect);
+
+        QPlayer p = new QPlayer("p");//QPlayers.players;
+        List<Player> players = query.from(p).where(p.gameid.eq(2209)).list(
+                Projections.bean(Player.class, new BeanPath<Player>(Player.class,"")));
+
+        for(Player player : players) {
+            System.out.print(player);
+
+        }
+    }
+
+    private static void queryWithTuples(Connection connection, SQLTemplates dialect) {
+        SQLQuery query = new SQLQueryImpl(connection, dialect);
+        QPlayer p = new QPlayer("p");
+        List<Tuple> tuples = query.from(p).where(p.gameid.eq(2209))
+                .list(new QTuple(p.gameid, p.name, p.serverid, p.guildid));
+
+        for(Tuple t : tuples) {
+            System.out.print("gameid=" + t.get(p.gameid) + "\n");
+            System.out.print("name=" + t.get(p.name) + "\n");
+            System.out.print("server=" + t.get(p.serverid) + "\n");
+            System.out.print("guildid=" + t.get(p.guildid) + "\n");
+            System.out.print("****************************\n");
+        }
+    }
 }
--- src/main/java/org/faboo/test/querydsl/model/Player.java
+++ src/main/java/org/faboo/test/querydsl/model/Player.java
@@ -1,7 +1,66 @@
 package org.faboo.test.querydsl.model;
 
+import java.util.Date;
+
 /**
  * User: br
  */
 public class Player {
+
+    private Long gameID;
+    private String server;
+    private String name;
+    private Date upgraded;
+    private Long guildID;
+
+    public Long getGameID() {
+        return gameID;
+    }
+
+    public void setGameID(Long gameID) {
+        this.gameID = gameID;
+    }
+
+    public String getServer() {
+        return server;
+    }
+
+    public void setServer(String server) {
+        this.server = server;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getUpgraded() {
+        return upgraded;
+    }
+
+    public void setUpgraded(Date upgraded) {
+        this.upgraded = upgraded;
+    }
+
+    public Long getGuildID() {
+        return guildID;
+    }
+
+    public void setGuildID(Long guildID) {
+        this.guildID = guildID;
+    }
+
+    @Override
+    public String toString() {
+        return "Player{" +
+                "gameID=" + gameID +
+                ", server='" + server + '\'' +
+                ", name='" + name + '\'' +
+                ", upgraded=" + upgraded +
+                ", guildID=" + guildID +
+                '}';
+    }
 }
