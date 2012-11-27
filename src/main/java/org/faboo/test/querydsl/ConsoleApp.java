da79d06062fb354159513c1fd2f231a97915d6bc
Eddie <taseroth@gmail.com>
2012-11-27 22:50:49 +0100

    MappingProjection and code comments

--- src/main/java/org/faboo/test/querydsl/ConsoleApp.java
+++ src/main/java/org/faboo/test/querydsl/ConsoleApp.java
@@ -5,6 +5,8 @@ import com.mysema.query.sql.PostgresTemplates;
 import com.mysema.query.sql.SQLQuery;
 import com.mysema.query.sql.SQLQueryImpl;
 import com.mysema.query.sql.SQLTemplates;
+import com.mysema.query.types.Expression;
+import com.mysema.query.types.MappingProjection;
 import com.mysema.query.types.QTuple;
 import org.faboo.test.querydsl.generated.beans.eddie2.Player2;
 import org.faboo.test.querydsl.generated.qtypes.eddie2.QPlayer2;
@@ -15,7 +17,9 @@ import java.sql.SQLException;
 import java.util.List;
 
 /**
- * User: br
+ * First take at QueryDSL, loading players from a database, looking at different ways of constructing domain objects.
+ *
+ * User: bert
  */
 public class ConsoleApp {
 
@@ -30,6 +34,7 @@ public class ConsoleApp {
 
             queryWithTuples(connection, dialect);
             queryWithBeanProjection(connection,dialect);
+            queryWithMappingProjection(connection,dialect);
 
         } catch (Exception e) {
             e.printStackTrace();
@@ -45,20 +50,62 @@ public class ConsoleApp {
 
     }
 
+    /**
+     * Query for players and project (construct) the player from the query result via MappingProjection.
+     *
+     * @param connection JDBC Connection
+     * @param dialect the dialect / template to use on the connection
+     */
+    private static void queryWithMappingProjection(Connection connection, SQLTemplates dialect) {
+
+        SQLQuery query = new SQLQueryImpl(connection, dialect);
+
+        final QPlayer2 p2 = QPlayer2.player;
+
+        List<Player2> player2s = query.from(p2).where(p2.gameid.eq(2209)).list(
+                new Player2MappingProjection(p2.gameid, p2.serverid, p2.name,p2.closed,p2.guildid, p2.upgraded));
+
+        System.out.print(" **** mapped via mapping projection ****\n");
+        for(Player2 player : player2s) {
+            System.out.print(player + "\n");
+        }
+
+    }
+
+
+    /**
+     * Query for players and project (construct) the player from the query result via Bean Projection.
+     *
+     * The bean to project to must have the same properties as the generated QClass.
+     *
+     * @param connection JDBC Connection
+     * @param dialect the dialect / template to use on the connection
+     */
     private static void queryWithBeanProjection(Connection connection, SQLTemplates dialect) {
+
         SQLQuery query = new SQLQueryImpl(connection, dialect);
 
         QPlayer2 p = QPlayer2.player;
         List<Player2> players = query.from(p).where(p.gameid.eq(2209)).list(p);
 
+        System.out.print(" **** mapped via bean projection ****\n");
         for(Player2 player : players) {
-            System.out.print(player);
-
+            System.out.print(player + "\n");
         }
     }
 
+    /**
+     * Query for players and project (construct) the player from the query result via Tuples.
+     *
+     * Tupes are type refactoring save versions of ResultSets.
+     *
+     * @param connection JDBC Connection
+     * @param dialect the dialect / template to use on the connection
+     */
     private static void queryWithTuples(Connection connection, SQLTemplates dialect) {
+
         SQLQuery query = new SQLQueryImpl(connection, dialect);
+
         QPlayer2 p = new QPlayer2("p");
         List<Tuple> tuples = query.from(p).where(p.gameid.eq(2209))
                 .list(new QTuple(p.gameid, p.name, p.serverid, p.guildid));
@@ -69,6 +116,34 @@ public class ConsoleApp {
             System.out.print("serverid=" + t.get(p.serverid) + "\n");
             System.out.print("guildid=" + t.get(p.guildid) + "\n");
             System.out.print("****************************\n");
+        }
+    }
+
+
+    /**
+     * Mapping class to project from the query result back to the domain object.
+     *
+     */
+    private static class Player2MappingProjection extends MappingProjection<Player2> {
+
+
+        public Player2MappingProjection(Expression<?>...args) {
+            super(Player2.class, args);
+        }
+
+        @Override
+        protected Player2 map(Tuple row) {
+
+            QPlayer2 p2 = QPlayer2.player;
+
+            Player2 player2 = new Player2();
+            player2.setClosed(row.get(p2.closed));
+            player2.setGameid(row.get(p2.gameid));
+            player2.setGuildid(row.get(p2.guildid));
+            player2.setName(row.get(p2.name));
+            player2.setServerid(row.get(p2.serverid));
+            player2.setUpgraded(row.get(p2.upgraded));
+            return player2;
         }
     }
 }
